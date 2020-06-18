fa1e966c37cf42790c1bbadc41a852ec20ad8835
3hak <al.miroshnychenko@gmail.com>
2020-06-18 12:03:14 +0300

    WIP:add test

---
+++ server/server_test.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"context"
+	"github.com/3hajk/grpc-project/proto"
+	"log"
+	"os"
+	"testing"
+	"time"
+)
+
+func TestFetch(t *testing.T) {
+	ctx, logger, cancel := initTest("TestApproveAfterExpire", 60)
+	defer cancel()
+
+	grpc := newServer()
+
+	request := &proto.FetchRequest{
+		Url: "http://127.0.0.1/data.csv",
+	}
+
+	response, err := grpc.Fetch(ctx, request)
+	if err != nil {
+		logger.Fatal("", err.Error())
+	}
+	logger.Println("resp", response.String())
+
+}
+
+func initTest(testName string, testDuration int64) (ctx context.Context, logger *log.Logger, cancel func()) {
+	logger = log.New(os.Stdout, testName, log.Ldate|log.Ltime|log.Lshortfile)
+	ctx, cancel = context.WithTimeout(context.Background(), time.Duration(testDuration)*time.Second)
+
+	return ctx, logger, cancel
+}
