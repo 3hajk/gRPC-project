95abd45f184eb5771f89a0f15c50b4b0e83ee50b
3hak <al.miroshnychenko@gmail.com>
2020-06-18 10:51:00 +0300

    WIP: add server

--- Makefile
+++ Makefile
@@ -5,17 +5,34 @@ VCS_REF := $(shell git log -1 --pretty=%h)
 NAME := $(shell basename `git rev-parse --show-toplevel`)
 VENDOR := $(shell whoami)
 
+proto-gen:
+	find . -name "*.proto" -exec \
+	protoc -I. \
+	-I${GOPATH}/src \
+	--go_out=plugins=grpc:. \
+	--proto_path=. "{}" \;
 
 print:
-    @echo VERSION=${VERSION}
-    @echo BUILD_DATE=${BUILD_DATE}
-    @echo VCS_URL=${VCS_URL}
-    @echo VCS_REF=${VCS_REF}
-    @echo NAME=${NAME}
-    @echo VENDOR=${VENDOR}
+	@echo VERSION=${VERSION}
+	@echo BUILD_DATE=${BUILD_DATE}
+	@echo VCS_URL=${VCS_URL}
+	@echo VCS_REF=${VCS_REF}
+	@echo NAME=${NAME}
+	@echo VENDOR=${VENDOR}
+
+build-server:
+	cd server && docker build -t grpc-project/grpc-server --build-arg VERSION="${VERSION}" \
+    --build-arg BUILD_DATE="${BUILD_DATE}" \
+    --build-arg VCS_URL="${VCS_URL}" \
+    --build-arg VCS_REF="${VCS_REF}" \
+    --build-arg NAME="${NAME}" \
+    --build-arg VENDOR="${VENDOR}" .
 
 up:
 	@docker-compose up -d
 
 down:
 	@docker-compose down
+
+scale:
+    @docker-compose up -d --scale server=3
---
+++ server/server.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"context"
+	"encoding/csv"
+	pb "github.com/grpc-project/proto"
+	"google.golang.org/grpc"
+	"log"
+	"net"
+	"net/http"
+	"os"
+	"sync"
+)
+
+type routeGuideServer struct {
+	pb.UnimplementedServerServer
+	mu sync.Mutex // protects routeNotes
+}
+
+func newServer() *routeGuideServer {
+	s := &routeGuideServer{}
+	return s
+}
+
+func saveData(data [][]string) error {
+	return nil
+}
+
+func (s *routeGuideServer) Fetch(ctx context.Context, req *pb.FetchRequest) (*pb.FetchResponse, error) {
+	resp, err := http.Get(req.Url)
+	if err != nil {
+		return &pb.FetchResponse{Error: err.Error()}, err
+	}
+	defer resp.Body.Close()
+	reader := csv.NewReader(resp.Body)
+	reader.Comma = ';'
+	data, err := reader.ReadAll()
+	if err != nil {
+		return &pb.FetchResponse{Error: err.Error()}, err
+	}
+	saveData(data)
+	return &pb.FetchResponse{}, nil
+}
+
+func (s *routeGuideServer) List(ctx context.Context, req *pb.ListRequest) (*pb.ListResponse, error) {
+
+	return nil, nil
+}
+
+func main() {
+
+	port := os.Getenv("PORT")
+
+	listener, err := net.Listen("tcp", port)
+	if err != nil {
+		log.Fatalf("failed to listen: %s", err.Error())
+	}
+	opts := []grpc.ServerOption{}
+	grpcServer := grpc.NewServer(opts...)
+	defer grpcServer.Stop()
+
+	pb.RegisterServerServer(grpcServer, newServer())
+	grpcServer.Serve(listener)
+}
